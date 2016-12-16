0515136f36bdd8ef40cfe6e5bea45e144ee0f9f2
alexeyeva <kateryna.alexeyeva@gmail.com>
2016-12-16 09:55:59 -0500

    fix

---
+++ total_increasing_or_decreasing_numbers.rb
@@ -0,0 +1,33 @@
+# x = gets.to_i
+
+# def total_inc_dec(x)
+#   number = 10 ** x
+#   for i in 1..number
+#   	if i == 1
+#   		@count = 1
+#   	else
+#   		@count += 1 if (i.to_s.chars == i.to_s.chars.sort) || (i.to_s.chars == i.to_s.chars.sort.reverse)
+#   	end
+
+#   	i += 1
+#   end
+
+#   return @count
+# end
+
+def total_inc_dec(x)
+	number = 10 ** x
+	
+
+
+	@count = 
+
+end
+
+# total_inc_dec(0) #1
+# total_inc_dec(1) #10
+# puts total_inc_dec(2) #100
+# puts total_inc_dec(3) #475
+# puts total_inc_dec(4) #1675
+# puts total_inc_dec(10) #1675
+# total_inc_dec(x)
